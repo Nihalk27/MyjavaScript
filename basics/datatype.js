89d17eb947eb9484e65aec698f14338a1f4d4607
Nihal Kondukwar <119446824+Nihalk27@users.noreply.github.com>
2025-05-31 06:06:14 +0000

    understanding of datatype

---
+++ basics/datatype.js
@@ -0,0 +1,31 @@
+"use strict" //treat all js code as newer version where as new version treat all as a use strict
+ 
+//alert () is used in browser not in nodejs 
+
+//we can use the ; as well when writting multiple statement in a single line
+ console.log("hello");console.log("World") //not a good practice
+
+ let name = "Nihal" //string data type
+ let age = 25 //int data type
+ let isLoggedIn = false // boolean 
+
+ /*
+ type    => range
+
+ primitive data types 
+ These types are typically built into the language and are used to represent simple values
+ Number => 2 to power 53
+ bigint
+ boolean => larger value than int
+ string => ' ' or ""
+ null => standalone value , it has significance as empty but it is not similar to the undefined 
+ undefined => not declared but also not null 
+ symbol => mostly used in react  
+
+object =>
+
+*/
+console.log(typeof "Nihal")
+console.log(typeof null) // it give as 'Object'
+console.log(typeof undefined) // where as it is undefined
+console.log(typeof 23 )
